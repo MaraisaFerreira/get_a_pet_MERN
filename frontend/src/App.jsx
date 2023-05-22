3c952655f0300ef194aba9a7cb68f0324837c1e6
Maraisa Ferreira <ferreira.maraisacristina@gmail.com>
2023-05-22 11:52:58 -0300

    main files & folders created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,19 @@
-import './App.css';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+
+/* pages */
+import Home from './components/pages/Home';
+import Login from './components/pages/auth/Login';
+import Register from './components/pages/auth/Register';
 
 function App() {
 	return (
-		<div>
-			<h1>Get a Pet</h1>
-		</div>
+		<Router>
+			<Routes>
+				<Route path='/login' element={<Login />} />
+				<Route path='/register' element={<Register />} />
+				<Route path='/' element={<Home />} />
+			</Routes>
+		</Router>
 	);
 }
 
