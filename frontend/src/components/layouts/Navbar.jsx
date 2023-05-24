33390839a4c1f6820e89afc795a5805a8fd3439c
Maraisa Ferreira <ferreira.maraisacristina@gmail.com>
2023-05-24 16:40:28 -0300

    alter navbar on login

--- frontend/src/components/layouts/Navbar.jsx
+++ frontend/src/components/layouts/Navbar.jsx
@@ -1,3 +1,4 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
 /* assets */
@@ -6,7 +7,12 @@ import Logo from '../../assets/img/cat.png';
 /* css */
 import style from './Navbar.module.css';
 
+/* context */
+import { Context } from '../../context/UserContext';
+
 function Navbar() {
+	const { authenticated } = useContext(Context);
+
 	return (
 		<nav className={style.navbar}>
 			<div className={style.navbar_logo}>
@@ -17,12 +23,20 @@ function Navbar() {
 				<li>
 					<Link to='/'>Home</Link>
 				</li>
-				<li>
-					<Link to='/login'>Login</Link>
-				</li>
-				<li>
-					<Link to='/register'>Cadastrar</Link>
-				</li>
+				{authenticated ? (
+					<>
+						<p>Logado</p>
+					</>
+				) : (
+					<>
+						<li>
+							<Link to='/login'>Login</Link>
+						</li>
+						<li>
+							<Link to='/register'>Cadastrar</Link>
+						</li>
+					</>
+				)}
 			</ul>
 		</nav>
 	);
