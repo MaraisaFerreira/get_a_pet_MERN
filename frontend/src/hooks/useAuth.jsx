7772cdcca53a374e5f3b866255df4197bcaacb62
Maraisa Ferreira <ferreira.maraisacristina@gmail.com>
2023-05-24 13:20:31 -0300

    authentication & localStorage

--- frontend/src/hooks/useAuth.jsx
+++ frontend/src/hooks/useAuth.jsx
@@ -1,9 +1,24 @@
 import api from '../utils/api';
 
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import useFlashMessage from './useFlashMessage';
 
 export default function useAuth() {
 	const { setFlashMessage } = useFlashMessage();
+
+	const [authenticated, setAuthenticated] = useState(false);
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		const token = localStorage.getItem('token');
+
+		if (token) {
+			api.defaults.headers.Authorization = `Bearer ${JSON.parse(token)}`;
+			setAuthenticated(true);
+		}
+	}, []);
 
 	async function register(user) {
 		let msgTxt = 'Cadastro realizado com sucesso!';
@@ -13,6 +28,7 @@ export default function useAuth() {
 			const data = await api.post('/users/register', user).then((response) => {
 				return response.data;
 			});
+			await authUser(data);
 		} catch (err) {
 			msgTxt = err.response.data.message;
 			type = 'error';
@@ -21,5 +37,11 @@ export default function useAuth() {
 		setFlashMessage(msgTxt, type);
 	}
 
-	return { register };
+	async function authUser(data) {
+		setAuthenticated(true);
+		localStorage.setItem('token', JSON.stringify(data.token));
+		navigate('/');
+	}
+
+	return { authenticated, register };
 }
